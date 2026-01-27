a738b685f5369fe74e4b6403908c7d681899787a
Enerhim <himsharma.contact@gmail.com>
2026-01-28 00:42:44 +0530

    Fashion MNIST 82.75%

--- main.c
+++ main.c
@@ -6,21 +6,21 @@ int main(void) {
   srand(time(NULL));
 
   uint64_t n_train_images, n_test_images, n_train_labels, n_test_labels;
-  Matrix X_train = load_mnist_dataset("./data/train-images-idx3-ubyte",
+  Matrix X_train = load_mnist_dataset("./fashion_dat/train-images-idx3-ubyte",
                                       &n_train_images, 60000);
-  Matrix y_labels = load_mnist_labels("./data/train-labels-idx1-ubyte",
+  Matrix y_labels = load_mnist_labels("./fashion_dat/train-labels-idx1-ubyte",
                                       &n_train_labels, X_train.rows);
-  Matrix X_test = load_mnist_dataset("./data/t10k-images-idx3-ubyte",
+  Matrix X_test = load_mnist_dataset("./fashion_dat/t10k-images-idx3-ubyte",
                                      &n_test_images, 10000);
-  Matrix y_test_labels = load_mnist_labels("./data/t10k-labels-idx1-ubyte",
-                                           &n_test_labels, X_test.rows);
+  Matrix y_test_labels = load_mnist_labels(
+      "./fashion_dat/t10k-labels-idx1-ubyte", &n_test_labels, X_test.rows);
 
   uint64_t units[] = {256, 128, 10};
   ActivationFunction activations[] = {relu, relu, softmax};
   NeuralNetwork net =
       createNetwork(3, units, activations, 784, crossEntropyLoss);
 
-  loadNetwork(&net, "model.gay");
+  loadNetwork(&net, "model_fashion.gay");
 
   Matrix y_train = mnist_hot_encode(y_labels);
   fitNetwork(&net, 0.1, 500, X_train, y_train, X_test, y_test_labels, 100);
@@ -29,7 +29,7 @@ int main(void) {
   float accuracy = calculate_mnist_accuracy(y_hat, y_test_labels);
   printf("Final Accuracy: %f", accuracy);
 
-  saveNetwork(&net, "model.gay");
+  saveNetwork(&net, "model_fashion.gay");
 
   freeNetwork(&net);
   freeMatrix(&X_train);
